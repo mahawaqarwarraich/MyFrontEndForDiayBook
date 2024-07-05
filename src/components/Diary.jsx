aaea969a8a32bf01bf44dffac3b1b015ec2713de
Maha <maha.waqar.warraich@gmail.com>
2024-07-05 23:59:58 +0500

    designed diary and footer component

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -2,8 +2,52 @@ import React from 'react';
 
 const Diary = () => {
     return (
-        <div>
-            <h1>This is Diary component.</h1>
+        <div className='pt-16 pb-11 bg-white flex flex-col justify-center items-center'>
+           <div className='text-center'>
+                <h1 className='font-bold text-3xl mb-6'>Diary Items</h1>
+                
+           </div>
+           <div className='bg-white border-2 border-gray-100 py-6 px-9 flex flex-col gap-6 w-[450px] rounded-lg mb-8'>
+                <div className='flex flex-col gap-1'>
+                    <label className='text-sm font-semibold' for='title'>Item Title</label>
+                    <input id='title' type='email' placeholder='Assignment due' className='rounded-lg border border-gray-400 px-2 py-3 text-sm bg-gray-50'/>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <label className='text-sm font-semibold' for='description'>Description</label>
+                    <input id='description' type='text' placeholder='Description about the diary item' className='rounded-lg border border-gray-400 px-2 py-3 text-sm bg-gray-50'/>
+                </div>
+              
+               
+                <button className='text-white font-semibold py-2 bg-blue-700 rounded-lg cursor-pointer'>Create Diary Item</button>
+              
+                
+           </div>
+           <div className='flex space-x-4'>
+            <div className='bg-white border border-gray-200 p-6 rounded-lg flex flex-col gap-3'>
+                <h1 className='font-bold text-xl'>Item 1</h1>
+                <p>This is description.</p>
+                <div className='flex space-x-2'>
+                    <button className='bg-blue-600 px-4 py-2 rounded-lg text-white font-semibold'>Edit</button>
+                    <button className='bg-red-600 px-4 py-2 rounded-lg text-white font-semibold'>Delete</button>
+                </div>
+            </div>
+            <div className='bg-white border border-gray-200 p-6 rounded-lg flex flex-col gap-3'>
+                <h1 className='font-bold text-xl'>Item 1</h1>
+                <p>This is description.</p>
+                <div className='flex space-x-2'>
+                    <button className='bg-blue-600 px-4 py-2 rounded-lg text-white font-semibold'>Edit</button>
+                    <button className='bg-red-600 px-4 py-2 rounded-lg text-white font-semibold'>Delete</button>
+                </div>
+            </div>
+            <div className='bg-white border border-gray-200 p-6 rounded-lg flex flex-col gap-3'>
+                <h1 className='font-bold text-xl'>Item 1</h1>
+                <p>This is description.</p>
+                <div className='flex space-x-2'>
+                    <button className='bg-blue-600 px-4 py-2 rounded-lg text-white font-semibold'>Edit</button>
+                    <button className='bg-red-600 px-4 py-2 rounded-lg text-white font-semibold'>Delete</button>
+                </div>
+            </div>
+           </div>
         </div>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer component.</h1>
+        <div className='px-24 py-8 bg-black'>
+            <div className='px-24 bg-black flex'>
+                <p className='text-white'><span className='text-gray-500'>© 2024 </span>courseCS.wordpress.com™<span className='text-gray-500'>. All Rights Reserved.</span></p>
+                <div className='flex space-x-5 ml-auto'>
+                    <a className='text-white font-semibold cursor-pointer hover:text-red-500'>About</a>
+                    <a className='text-white font-semibold cursor-pointer hover:text-red-500'>Privacy Policy</a>
+                    <a className='text-white font-semibold cursor-pointer hover:text-red-500'>Licensing</a>
+                    <a className='text-white font-semibold cursor-pointer hover:text-red-500'>Contact</a>
+                </div>
+            </div>
         </div>
     )
 }
