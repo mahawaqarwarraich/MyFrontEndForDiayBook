ce985ff6bc988ab01bb22d1f0062e219f78ded46
Maha <maha.waqar.warraich@gmail.com>
2024-07-05 13:51:45 +0500

    designed home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home component.</h1>
+        <div className='bg-gray-300 p-52 text-center'>
+           <h1 className='font-bold text-3xl mb-6'>Welcome to Diary Book</h1>
+           <p className='mb-10 text-lg'>you can create your own diary notes in our application. You can either create, delete, update diary items.</p>
+           <button className='btn bg-black text-white px-4 py-3 rounded-xl cursor-pointer'>View Our More Services</button>
         </div>
     )
 }
